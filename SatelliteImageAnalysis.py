e576af13aaf07891e4bb61a7170b5f111bfd70fa
WobiWanKenobi <tobias@witte-bayern.de>
2023-11-28 14:42:01 +0100

    first commit

---
+++ SatelliteImageAnalysis.py
@@ -0,0 +1,5 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import random_split
